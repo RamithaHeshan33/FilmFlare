a06c094e28971d9e54d243f1c897ec2f5077c582
Ramitha Heshan <ramithacampus@gmail.com>
2025-04-24 16:12:04 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
 
 app.use("/", (req, res) => {
     res.send("Hello World");
 });
 
-mongoose.connect("mongodb+srv://filmflare:CIUtgZbbZM6OpnZY@filmflare.smw9u4m.mongodb.net/")
+mongoose.connect(MONGO_URI)
 .then(() => {console.log("Connected to MongoDB")})
 .then(() => {app.listen(PORT);})
 .catch((err) => {console.log(err);})
